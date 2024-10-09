b872c91d1b33b64b884619fa8cece41dcd3fb28a
TeachMeTW <teachmetheways652@gmail.com>
2024-10-09 16:20:00 -0700

    Testing web updates

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ st.image(
     caption="Skibidi Toilet",
     use_column_width=True
 )
-
+st.markdown("---")
 video_file = open("skibidi.mp4", "rb")
 video_bytes = video_file.read()
 st.video(video_bytes)
