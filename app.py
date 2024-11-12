ccee5ddc0836924c8852aed27a8319212099f09c
Arya Sastry <atsastry@calpoly.edu>
2024-11-12 14:37:57 -0800

    Pyg

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import streamlit.components.v1 as components
-
+import pandas as pd
+import pygwalker as pyg 
 
 
 #sidebar
 st.sidebar.title("Navigation")
 page = st.sidebar.selectbox(
     "Choose a page:",
-    ("Project Description", "User Guide", "Data")
+    ("Project Description", "User Guide", "Data", "PygWalker Visualization")
 )
-st.title("Anamoly Detection Dashboard")
+
+# main title and description
+st.title("Anomaly Detection Dashboard")
 st.markdown(
     """
     <div style='text-align: center;'>
@@ -26,10 +29,29 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+
 # Add a space between the typewriter and the image
 st.markdown("---")
 st.image(
     "Cisco_logo.png",
     caption="Cisco Design",
     use_column_width=True)
+ 
 
+if page == "PygWalker Visualization":
+    st.header('Data Visualization with Pygwalker')
+    
+    df = pd.read_csv("/Users/aryasastry/Downloads/CalPoly_example_files 2/iot_telemetry.csv")
+    # pyg.walk(df) --> does not work because pygwalker not natively compatible with streamlit 
+
+    # Render Pygwalker visualization as HTML
+    walker_html = pyg.walk(df).to_html()
+    components.html(walker_html, height=600, scrolling=True)
+
+
+    # # Title for the Streamlit app
+    # st.title("PyGWalker with Streamlit - Interactive Example")
+
+    # # Display the dataset
+    # st.write("### Sample Data")
+    # st.dataframe(df)
